aaff6f738b792cf2a919fb20d588e07194bbf0da
Jelleinholland <668419@student.inholland.nl>
2022-04-08 18:23:55 +0200

    Update index.php

--- inf2a-mvc-master/app/views/article/index.php
+++ inf2a-mvc-master/app/views/article/index.php
@@ -32,8 +32,8 @@ include __DIR__ . '/../footer.php';
                     editbutton.innerText = "edit article";
                     deletebutton.innerText = "delete article";
 
-                    editbutton.onclick = window.location.href='/views/updatingarticle/index.php' + article.id;
-                    deletebutton.onclick = window.location.href='/views/deletingarticle/index.php' + article.id;
+                    editbutton.onclick = window.location.href='/article/addingarticle' + article.id;
+                    deletebutton.onclick = window.location.href='/article/deletingarticle' + article.id;
                     
                     posted_at_container.appendChild(posted_at); 
                     container.appendChild(header);
